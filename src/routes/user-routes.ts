50465f76607dbdb7392fd6af7c388698eb25a4f4
William Zheng <williamzheng2001@gmail.com>
2023-01-27 21:02:38 -0500

    Validate request body

--- src/routes/user-routes.ts
+++ src/routes/user-routes.ts
@@ -1,9 +1,26 @@
 import { Router } from 'express';
 import asyncHandler from 'express-async-handler';
+import { body } from 'express-validator';
+import { validateRequest } from '../middleware/validation-middleware';
 import { createUser } from '../controllers/user-controller';
 
 const router = Router();
 
-router.post('/', asyncHandler(createUser));
+router.post(
+  '/',
+  body('email', 'Email is required')
+    .trim()
+    .isEmail()
+    .withMessage('Email must be valid')
+    .normalizeEmail()
+    .escape(),
+  body('password', 'Password is required')
+    .trim()
+    .isLength({ min: 8 })
+    .withMessage('Password must be at least 8 characters')
+    .escape(),
+  validateRequest,
+  asyncHandler(createUser)
+);
 
 export default router;

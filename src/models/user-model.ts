e6e60a650c343e016ce26cfcec0768eb812d6c2f
William Zheng <williamzheng2001@gmail.com>
2023-01-28 15:14:05 -0500

    Hash password before saving to db

--- src/models/user-model.ts
+++ src/models/user-model.ts
@@ -1,4 +1,5 @@
 import { Schema, model } from 'mongoose';
+import bcrypt from 'bcrypt';
 
 interface IUser {
   email: string;
@@ -11,6 +12,12 @@ const userSchema = new Schema<IUser>({
 });
 
 userSchema.set('timestamps', true);
+
+userSchema.pre('save', async function (next) {
+  const SALT_ROUNDS = 10;
+  this.password = await bcrypt.hash(this.password, SALT_ROUNDS);
+  next();
+});
 
 const User = model<IUser>('User', userSchema);
 export default User;
